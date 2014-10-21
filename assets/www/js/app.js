6e797d09ac76387d9577ad53674fd6e128959d55
xifei <xifei@nfs.iscas.ac.cn>
2014-10-21 17:22:06 +0800

    Implementing View Routing

--- assets/www/js/app.js
+++ assets/www/js/app.js
@@ -1,42 +1,22 @@
 // We use an "Immediate Function" to initialize the application to avoid leaving anything behind in the global scope
 (function () {
 
-    //add later
-    //var homeTpl = Handlebars.compile($("#home-tpl").html());
-    //var employeeListTpl = Handlebars.compile($("#employee-list-tpl").html());
-
-    //add later
-    // function findByName() {
-    //     service.findByName($('.search-key').val()).done(function (employees) {
-    //         $('.content').html(employeeListTpl(employees));
-    //     });
-    // }
-
     /* ---------------------------------- Local Variables ---------------------------------- */
     HomeView.prototype.template = Handlebars.compile($("#home-tpl").html());
     EmployeeListView.prototype.template = Handlebars.compile($("#employee-list-tpl").html());
+    EmployeeView.prototype.template = Handlebars.compile($("#employee-tpl").html());
     var service = new EmployeeService();
     service.initialize().done(function () {
-        $('body').html(new HomeView(service).render().$el);
+        router.addRoute('', function() {
+            $('body').html(new HomeView(service).render().$el);
+        });
+        router.addRoute('employees/:id', function(id) {
+            service.findById(parseInt(id)).done(function(employee) {
+              $('body').html(new EmployeeView(employee).render().$el);
+            });
+        });
+        router.start();
     });
-    /* --------------------------------- Event Registration -------------------------------- */
-    // $('.search-key').on('keyup', findByName);
-    // $('.help-btn').on('click', function() {
-    //     alert("Employee Directory v3.4");
-    // });
-
-    /* ---------------------------------- Local Functions ---------------------------------- */
-    // function findByName() {
-    //     service.findByName($('.search-key').val()).done(function (employees) {
-    //         var l = employees.length;
-    //         var e;
-    //         $('.employee-list').empty();
-    //         for (var i = 0; i < l; i++) {
-    //             e = employees[i];
-    //             $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-    //         }
-    //     });
-    // }
 
     //add later
     document.addEventListener('deviceready', function () {
