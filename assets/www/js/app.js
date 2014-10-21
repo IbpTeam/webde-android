c335354bf784632280757158c545363783aef40d
xifei <xifei@nfs.iscas.ac.cn>
2014-10-21 15:35:17 +0800

    html is created by Handlebars Templates.

--- assets/www/js/app.js
+++ assets/www/js/app.js
@@ -1,6 +1,21 @@
 // We use an "Immediate Function" to initialize the application to avoid leaving anything behind in the global scope
 (function () {
 
+    //add later
+    var homeTpl = Handlebars.compile($("#home-tpl").html());
+    var employeeListTpl = Handlebars.compile($("#employee-list-tpl").html());
+
+    //add later
+    function renderHomeView() {
+        $('body').html(homeTpl());
+        $('.search-key').on('keyup', findByName);
+    }
+    function findByName() {
+        service.findByName($('.search-key').val()).done(function (employees) {
+            $('.content').html(employeeListTpl(employees));
+        });
+    }
+
     /* ---------------------------------- Local Variables ---------------------------------- */
     var service = new EmployeeService();
     service.initialize().done(function () {
@@ -14,18 +29,19 @@
     });
 
     /* ---------------------------------- Local Functions ---------------------------------- */
-    function findByName() {
-        service.findByName($('.search-key').val()).done(function (employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i = 0; i < l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    }
+    // function findByName() {
+    //     service.findByName($('.search-key').val()).done(function (employees) {
+    //         var l = employees.length;
+    //         var e;
+    //         $('.employee-list').empty();
+    //         for (var i = 0; i < l; i++) {
+    //             e = employees[i];
+    //             $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
+    //         }
+    //     });
+    // }
 
+    //add later
     document.addEventListener('deviceready', function () {
       if (navigator.notification) { // Override default HTML alert with native dialog
           window.alert = function (message) {
@@ -38,5 +54,5 @@
           };
       }
     }, false);
-
+    renderHomeView();
 }());
