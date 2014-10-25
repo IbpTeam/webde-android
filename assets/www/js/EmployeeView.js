329d8885fe37ce9538d6b5a0e239bfd7abe96cac
xifei <xifei@nfs.iscas.ac.cn>
2014-10-25 23:30:42 +0800

    cordova plugin add org.apache.cordova.geolocation, but fail.

--- assets/www/js/EmployeeView.js
+++ assets/www/js/EmployeeView.js
@@ -1,14 +1,29 @@
 var EmployeeView = function(employee) {
 
-  this.initialize = function() {
-      this.$el = $('<div/>');
-  };
+	this.initialize = function() {
+	    this.$el = $('<div/>');
+	    this.$el.on('click', this.addLocation);//'.media', .add-location-btn
+	};
 
-  this.render = function() {
-      this.$el.html(this.template(employee));
-      return this;
-  };
+	this.render = function() {
+	    this.$el.html(this.template(employee));
+	    return this;
+	};
 
-  this.initialize();
+	this.addLocation = function(event) {
+        console.log("in function addLocation");
+        event.preventDefault();
+        navigator.geolocation.getCurrentPosition(
+            function(position) {
+                alert(position.coords.latitude + ',' + position.coords.longitude);
+                console.log("position.coords.latitude + ',' + position.coords.longitude");
+            },
+            function() {
+                alert('Error getting location');
+                console.log("Error getting location");
+            });
+        return false;
+	};
 
+    this.initialize();
 }
