dd30ce3550d7f3c27f70624ad55c13b249146d70
xifei <xifei@nfs.iscas.ac.cn>
2014-10-25 23:55:00 +0800

    cordova plugin add org.apache.cordova.camera

--- assets/www/cordova_plugins.js
+++ assets/www/cordova_plugins.js
@@ -83,6 +83,34 @@ module.exports = [
         "merges": [
             ""
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.camera/www/CameraConstants.js",
+        "id": "org.apache.cordova.camera.Camera",
+        "clobbers": [
+            "Camera"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.camera/www/CameraPopoverOptions.js",
+        "id": "org.apache.cordova.camera.CameraPopoverOptions",
+        "clobbers": [
+            "CameraPopoverOptions"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.camera/www/Camera.js",
+        "id": "org.apache.cordova.camera.camera",
+        "clobbers": [
+            "navigator.camera"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.camera/www/CameraPopoverHandle.js",
+        "id": "org.apache.cordova.camera.CameraPopoverHandle",
+        "clobbers": [
+            "CameraPopoverHandle"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -92,7 +120,8 @@ module.exports.metadata =
     "org.apache.cordova.console": "0.2.11",
     "org.apache.cordova.dialogs": "0.2.10",
     "org.apache.cordova.geolocation": "0.3.10",
-    "org.apache.cordova.contacts": "0.2.14"
+    "org.apache.cordova.contacts": "0.2.14",
+    "org.apache.cordova.camera": "0.3.3"
 }
 // BOTTOM OF METADATA
 });
