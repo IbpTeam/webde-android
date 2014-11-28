db605b4c26b9bb087ccb8c6fd84ea6d363b6dac7
xifei <xifei@nfs.iscas.ac.cn>
2014-11-28 15:20:39 +0800

    test study.html ok

--- assets/webui/custom.js
+++ assets/webui/custom.js
@@ -1,13 +1,13 @@
 function updateLogView(){
-    var panel_nsd_logview = $('#afui #content #panel_nsd_logview');
-    var panel_nsd_logview_form = $(panel_nsd_logview).find('#panel_nsd_logview_form');
-    var panel_nsd_logview_form_textarea = $(panel_nsd_logview_form).find('textarea');
-    var panel_nsd_logview_history = $(panel_nsd_logview).find('#panel_nsd_logview_history');
-    var content = panel_nsd_logview_form_textarea.val();
-    // console.log($(panel_nsd_logview_history).html());
+    // var panel_nsd_logview = $('#afui #content #panel_nsd_logview');
+    // var panel_nsd_logview_form = $(panel_nsd_logview).find('#panel_nsd_logview_form');
+    // var panel_nsd_logview_form_textarea = $(panel_nsd_logview_form).find('textarea');
+    var panel_nsd_talk_history = $('#afui #content #panel_nsd_talk').find('ul');
+    var panel_nsd_talk_footer = $('#afui #panel_nsd_talk_footer').find('textarea');
+    var content = panel_nsd_talk_footer.val();
     if(content.length){
-        panel_nsd_logview_history.append($('<li></li>').html(content));
-        panel_nsd_logview_form_textarea.val('');
+        panel_nsd_talk_history.append($('<li></li>').html(content));
+        panel_nsd_talk_footer.val('');
     }else{
         alert("内容不能为空");
     }
