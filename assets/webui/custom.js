6993165220bf5dd3e1c2c68f641f2d4ef131596b
xifei <xifei@nfs.iscas.ac.cn>
2014-11-28 14:10:15 +0800

    fix log view.

--- assets/webui/custom.js
+++ assets/webui/custom.js
@@ -1,15 +1,18 @@
 function updateLogView(){
     var panel_nsd_logview = $('#afui #content #panel_nsd_logview');
-    var panel_nsd_logview_form = $(panel_nsd_logview).children('#panel_nsd_logview_form');
-    console.log( panel_nsd_logview);
-    console.log(panel_nsd_logview_form);
-    var panel_nsd_logview_form2 = $('#afui #content #panel_nsd_logview #panel_nsd_logview_form');
-    console.log(panel_nsd_logview_form2);
-    // var content = $(panel_nsd_logview_form).children('textarea').val();
-    // console.log("contetn: " + content);
+    var panel_nsd_logview_form = $(panel_nsd_logview).find('#panel_nsd_logview_form');
+    var panel_nsd_logview_form_textarea = $(panel_nsd_logview_form).find('textarea');
+    var panel_nsd_logview_history = $(panel_nsd_logview).find('#panel_nsd_logview_history');
+    var content = panel_nsd_logview_form_textarea.val();
+    // console.log($(panel_nsd_logview_history).html());
+    if(content.length){
+        panel_nsd_logview_history.append($('<li></li>').html(content));
+        panel_nsd_logview_form_textarea.val('');
+    }else{
+        alert("内容不能为空");
+    }
+    // console.log("content: " + content);
+    // panel_nsd_logview_form_textarea
     // $('#afui #content #panel_nsd_logview #panel_nsd_logview_form textarea').val("hello");
-    // console.log( panel_nsd_logview);
-    // console.log(panel_nsd_logview_form);
-    // console.log('in function updateLogView');
     // $(panel_nsd_logview_form).children('textarea').val('hellofdasf.');
 }
